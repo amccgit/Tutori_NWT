6eed5781e04f16bca6361bc49a634c515672b778
Senadtm87 <Senad_tm87@yahoo.com>
2014-03-20 00:11:26 +0100

    WCF rest
    CRUD za svaku tabelu

---
+++ WCF/WebSites/Oglasnik/App_Code/IService.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService" in both code and config file together.
+[ServiceContract]
+public interface IService
+{
+    #region REST_Korisnik
+    [OperationContract]
+    [WebGet(ResponseFormat=WebMessageFormat.Json, UriTemplate="/korisnik/{idkorisnika}")]
+    korisnik GetKorisnik(string idkorisnika);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json,UriTemplate="/unesikorisnika")]
+    void PostKorisnik(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method="PUT",RequestFormat=WebMessageFormat.Json,UriTemplate="/izmjenikorisnika")]
+    void PUTKorisnik(string streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisikorisnika")]
+    void DeleteKorisnik(Stream streamdata);
+    #endregion
+
+    #region REST_Tutor
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/tutor/{idtutora}")]
+    tutor GetTutor(string idtutora);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesitutora")]
+    void PostTutor(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjenitutora")]
+    void PUTTutor(string streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisitutor")]
+    void DeleteTutor(Stream streamdata);
+    #endregion
+
+    #region REST_Kategorija
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/kategorija/{idkategorija}")]
+    kategorija GETKategorija(string idkategorija);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesikategoriju")]
+    void PostKategorija(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjenikategoriju")]
+    void PUTKategorija(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisikategoriju")]
+    void DELETEKategorija(Stream streamdata);
+    #endregion
+
+    #region REST_Komentar
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/komentar/{idkomentara}")]
+    komentar GETKomentar(string idkomentara);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesikomentar")]
+    void PostKomentar(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjenikomentar")]
+    void PUTKomentar(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisikomentar")]
+    void DELETEKomentar(Stream streamdata);
+ #endregion
+
+    #region REST_Oglas
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/oglas/{idoglasa}")]
+    oglas GETOglas(string idoglasa);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesioglas")]
+    void PostOglas(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjenioglas")]
+    void PUTOglas(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisiOglas")]
+    void DELETEOglas(Stream streamdata);
+    #endregion
+
+    #region REST_poruka
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/poruka/{idporuka}")]
+    poruka GETPoruku(string idporuka);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesiporuku")]
+    void PostPoruku(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjeniporuku")]
+    void PUTPoruku(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisiporuku")]
+    void DELETEPoruku(Stream streamdata);
+    #endregion
+
+    #region REST_Strucnost
+    [OperationContract]
+    [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/strucnost/{idstrucnost}")]
+    strucnost GETStrucnost(string idstrucnost);
+    [OperationContract]
+    [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate = "/unesistrucnost")]
+    void PostStrucnost(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "PUT", RequestFormat = WebMessageFormat.Json, UriTemplate = "/izmjenistrucnost")]
+    void PUTStrucnost(Stream streamdata);
+    [OperationContract]
+    [WebInvoke(Method = "DELETE", RequestFormat = WebMessageFormat.Json, UriTemplate = "/obrisistrucnost")]
+    void DELETEStrucnost(Stream streamdata);
+    #endregion
+}
---
+++ WCF/WebSites/Oglasnik/App_Code/Service.cs
@@ -0,0 +1,576 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+public class Service : IService
+{
+    #region REST_Korisnik
+    public korisnik GetKorisnik(string idkorisnika)
+    {
+        try
+        {
+           korisnik k = new korisnik();
+           bazaEntities db = new bazaEntities();
+           int idkorisnik = Convert.ToInt32(idkorisnika);
+         var  rezultat = db.korisnik.SingleOrDefault(x => x.idkorisnik == idkorisnik);
+            
+         k.idkorisnik = rezultat.idkorisnik;
+         k.ime = rezultat.ime;
+         k.prezime = rezultat.prezime;
+         k.username = rezultat.username;
+         k.password = rezultat.password;       
+        
+         k.profil = rezultat.profil;
+         k.telefon = rezultat.telefon;
+         k.email = rezultat.email;
+         k.boraviste = rezultat.boraviste;
+
+           
+            return k;
+        }
+        catch (Exception e) {
+                              Console.Write(e.StackTrace.ToString());
+                               return null; }
+    }
+
+
+    public void PostKorisnik(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+      
+    
+        
+       var serializer = new JavaScriptSerializer();
+   korisnik novi =    serializer.Deserialize<korisnik>(res);
+      bazaEntities db = new bazaEntities();
+      
+          db.korisnik.Add(novi);
+          db.SaveChanges();
+ 
+    }
+
+
+    public void PUTKorisnik(string streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        korisnik novi = serializer.Deserialize<korisnik>(res);
+        bazaEntities db = new bazaEntities();
+        db.korisnik.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+
+    public void DeleteKorisnik(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        korisnik novi = serializer.Deserialize<korisnik>(res);
+        bazaEntities db = new bazaEntities();
+        db.korisnik.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+    #endregion
+
+
+
+    #region REST_Tutor
+    public tutor GetTutor(string idtutora)
+    {
+        try
+        {
+            tutor k = new tutor();
+            bazaEntities db = new bazaEntities();
+            int idtutor = Convert.ToInt32(idtutora);
+            var rezultat = db.tutor.SingleOrDefault(x => x.idtutor == idtutor);
+            
+            k.idtutor = rezultat.idtutor;
+            k.ime = rezultat.ime;
+            k.prezime = rezultat.prezime;
+            k.username = rezultat.username;
+            k.password = rezultat.password;
+            k.kvalifikacija = rezultat.kvalifikacija;
+            k.profil = rezultat.profil;
+            k.telefon = rezultat.telefon;
+            k.email = rezultat.email;
+            k.boraviste = rezultat.boraviste;
+            k.opis = rezultat.opis;
+
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+
+    public void PostTutor(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        tutor novi = serializer.Deserialize<tutor>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.tutor.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTTutor(string streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        tutor novi = serializer.Deserialize<tutor>(res);
+        bazaEntities db = new bazaEntities();
+        db.tutor.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    public void DeleteTutor(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        tutor novi = serializer.Deserialize<tutor>(res);
+        bazaEntities db = new bazaEntities();
+        db.tutor.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+#endregion
+
+    #region REST_Kategorija
+    public kategorija GETKategorija(string idkategorija)
+    {
+        try
+        {
+            kategorija k = new kategorija();
+            bazaEntities db = new bazaEntities();
+            int idkategorije = Convert.ToInt32(idkategorija);
+            var rezultat = db.kategorija.SingleOrDefault(x => x.idkategorija == idkategorije);
+           
+            k.idkategorija = rezultat.idkategorija;
+            k.nazivKategorije = rezultat.nazivKategorije;
+            k.opis = rezultat.opis;
+           
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+    public void PostKategorija(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        kategorija novi = serializer.Deserialize<kategorija>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.kategorija.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTKategorija(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        kategorija novi = serializer.Deserialize<kategorija>(res);
+        bazaEntities db = new bazaEntities();
+        db.kategorija.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    public void DELETEKategorija(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        kategorija novi = serializer.Deserialize<kategorija>(res);
+        bazaEntities db = new bazaEntities();
+        db.kategorija.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+    #endregion
+
+
+    #region REST_Komentar
+    public komentar GETKomentar(string idkomentara)
+    {
+        try
+        {
+            komentar k = new komentar();
+            bazaEntities db = new bazaEntities();
+            int idkomentar = Convert.ToInt32(idkomentara);
+            var rezultat = db.komentar.SingleOrDefault(x => x.idkomentar == idkomentar);
+
+            k.idkomentar = rezultat.idkomentar;
+            k.korisnik_idkorisnika = rezultat.korisnik_idkorisnika;
+            k.naslov = rezultat.naslov;
+            k.tekst = rezultat.tekst;
+            k.datumKreiranja = rezultat.datumKreiranja;
+            k.datumAzuriranja = rezultat.datumAzuriranja;
+            k.oglas_idoglasa = rezultat.oglas_idoglasa;
+
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+    public void PostKomentar(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        komentar novi = serializer.Deserialize<komentar>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.komentar.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTKomentar(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        komentar novi = serializer.Deserialize<komentar>(res);
+        bazaEntities db = new bazaEntities();
+        db.komentar.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+    public void DELETEKomentar(Stream streamdata) {
+
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        komentar novi = serializer.Deserialize<komentar>(res);
+        bazaEntities db = new bazaEntities();
+        db.komentar.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+    #endregion
+
+
+    #region REST_Oglas
+    public oglas GETOglas(string idoglasa)
+    {
+        try
+        {
+            oglas k = new oglas();
+            bazaEntities db = new bazaEntities();
+            int idoglas = Convert.ToInt32(idoglasa);
+            var rezultat = db.oglas.SingleOrDefault(x => x.idoglas == idoglas);
+
+            k.idoglas = rezultat.idoglas;
+            k.naslov = rezultat.naslov;
+            k.tekst = rezultat.tekst;
+            k.datumKreiranja = rezultat.datumKreiranja;
+            k.datumAzuriranja = rezultat.datumAzuriranja;
+            k.tutor_idtutor = rezultat.tutor_idtutor;
+            k.kategorija_idkategorija = rezultat.kategorija_idkategorija;
+            k.cijena = rezultat.cijena;
+            k.portret = rezultat.portret;
+            k.strucnost_idstrucnost = rezultat.strucnost_idstrucnost;
+
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+    public void PostOglas(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        oglas novi = serializer.Deserialize<oglas>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.oglas.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTOglas(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        oglas novi = serializer.Deserialize<oglas>(res);
+        bazaEntities db = new bazaEntities();
+        db.oglas.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    public void DELETEOglas(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        oglas novi = serializer.Deserialize<oglas>(res);
+        bazaEntities db = new bazaEntities();
+        db.oglas.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+    #endregion
+
+    #region REST_Poruka
+    public poruka GETPoruku(string idporuka)
+    {
+        try
+        {
+            poruka k = new poruka();
+            bazaEntities db = new bazaEntities();
+            int idporuke = Convert.ToInt32(idporuka);
+            var rezultat = db.poruka.SingleOrDefault(x => x.idporuka == idporuke);
+
+            k.idporuka = rezultat.idporuka;
+            k.naslov = rezultat.naslov;
+            k.tekst = rezultat.tekst;
+            k.datum = rezultat.datum;
+            k.korisnik_idkorisnik = rezultat.korisnik_idkorisnik;
+            k.tutor_idtutor = rezultat.tutor_idtutor;
+            
+
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+    public void PostPoruku(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        poruka novi = serializer.Deserialize<poruka>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.poruka.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTPoruku(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        poruka novi = serializer.Deserialize<poruka>(res);
+        bazaEntities db = new bazaEntities();
+        db.poruka.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    public void DELETEPoruku(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        poruka novi = serializer.Deserialize<poruka>(res);
+        bazaEntities db = new bazaEntities();
+        db.poruka.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+    #endregion
+
+    #region REST_Strucnost
+    public strucnost GETStrucnost(string idstrucnost)
+    {
+        try
+        {
+            strucnost k = new strucnost();
+            bazaEntities db = new bazaEntities();
+            int idstrucnosti = Convert.ToInt32(idstrucnost);
+            var rezultat = db.strucnost.SingleOrDefault(x => x.idstrucnost == idstrucnosti);
+
+            k.idstrucnost = rezultat.idstrucnost;
+            k.iskustvo = rezultat.iskustvo;
+
+            return k;
+        }
+        catch (Exception e)
+        {
+            Console.Write(e.StackTrace.ToString());
+            return null;
+        }
+    }
+
+    public void PostStrucnost(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        strucnost novi = serializer.Deserialize<strucnost>(res);
+        bazaEntities db = new bazaEntities();
+
+        db.strucnost.Add(novi);
+        db.SaveChanges();
+    }
+
+    public void PUTStrucnost(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        strucnost novi = serializer.Deserialize<strucnost>(res);
+        bazaEntities db = new bazaEntities();
+        db.strucnost.Attach(novi);
+        db.Entry(novi).State = EntityState.Modified;
+        db.SaveChanges();
+    }
+
+    public void DELETEStrucnost(Stream streamdata)
+    {
+        StreamReader reader = new StreamReader(streamdata);
+        string res = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+
+
+
+        var serializer = new JavaScriptSerializer();
+        strucnost novi = serializer.Deserialize<strucnost>(res);
+        bazaEntities db = new bazaEntities();
+        db.strucnost.Attach(novi);
+        db.Entry(novi).State = EntityState.Deleted;
+        db.SaveChanges();
+    }
+#endregion
+}
